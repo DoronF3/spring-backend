87c1c01895b93db32cba916cbec43b1893cf6dfd
Doron <62395246+DoronF3@users.noreply.github.com>
2024-01-03 13:51:53 +0200

    Delete src directory

--- src/main/java/nice/assignment/backend/repo/RichPersonRepository.java
+++
@@ -1,7 +0,0 @@
-package nice.assignment.backend.repo;
-
-import nice.assignment.backend.model.RichPerson;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RichPersonRepository extends CrudRepository<RichPerson,Long> {
-}
